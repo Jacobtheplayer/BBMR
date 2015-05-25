f918100d200d2f7d1e25bc4b5782f48346160444
Jacobtheplayer <jacobfast9@gmail.com>
2015-05-25 12:39:58 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -34,15 +34,15 @@
 
          */
 
-        bot.commands.baconCommand = {
-            command: 'ip',  //The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.pointsCommand = {
+            command: 'points',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("/me The EclipseCraft server ip is: 109.167.58.86");
+                    API.sendChat(&u + " has" + + "points");
                 }
             }
         };
